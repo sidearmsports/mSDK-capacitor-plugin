892467d2b352b0adbaa0ccb0c1dd6b95976f46db
njrom <nick@njrom.com>
2021-07-29 12:32:45 -0400

    Fixes typo in plugin name

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import { registerPlugin } from '@capacitor/core';
 
 import type { PaciolanSdkPlugin } from './definitions';
 
-const Fanmaker = registerPlugin<PaciolanSdkPlugin>('Paciolan', {
+const PaciolanSdk = registerPlugin<PaciolanSdkPlugin>('Paciolan', {
   web: () => import('./web').then(m => new m.PaciolanSdkPluginWeb()),
 });
 
 export * from './definitions';
-export { Fanmaker };
+export { PaciolanSdk };
